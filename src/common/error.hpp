830134a5d6ad7d304db203fdc89beb027b8409e4
Samuel Malec <malec293@gmail.com>
2026-07-24 22:34:36 +0200

    add cthu ir

---
+++ src/common/error.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <ostream>
+#include <stdexcept>
+
+template< typename... Args >
+inline void error( Args&&... args )
+{
+    std::ostringstream out;
+    ( out << ... << std::forward< Args >( args ) );
+    throw std::runtime_error( out.str() );
+}
